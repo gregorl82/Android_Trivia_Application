62d9a329814a7774d0d6a5e510a4120bbbaf38e3
Gregor <gregorlennie@hotmail.co.uk>
2020-04-12 15:31:02 +0100

    Opens QuestionActivity when start button is clicked

--- app/src/main/java/com/example/android/qwizzr/MainActivity.kt
+++ app/src/main/java/com/example/android/qwizzr/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.android.qwizzr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mStartButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStartButton = findViewById(R.id.btn_main_start)
+        mStartButton.setOnClickListener { view ->
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
